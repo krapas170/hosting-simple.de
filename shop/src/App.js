97f47abf925eacbd3fa23435c888c0e0e29594fe
Pascal Kray <57595818+krapas170@users.noreply.github.com>
2023-11-29 11:49:23 +0100

    Update App.js

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+
+import React, { useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import LoginPage from "./sites/Login";
+import Register1Page from "./sites/Register1";
+import GameAuswahlPage from "./sites/GameAuswahl";
+
+import "./App.css";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("login"); // State für die aktuelle Seite
+
+  const nextPage = (page) => {
+    setCurrentPage(page); // Funktion zum Wechseln zur nächsten Seite
+  };
+
+  let pageComponent;
+
+  if (currentPage === "login") {
+    pageComponent = <LoginPage nextPage={nextPage} />;
+  } else if (currentPage === "server-auswahl") {
+    pageComponent = <GameAuswahlPage nextPage={nextPage} />;
+  } else if (currentPage === "registrieren") {
+    pageComponent = <Register1Page />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <div>{pageComponent}</div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
