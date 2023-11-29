c1ae3a72d17fcdcc0e275b548b37b22f6e33b64b
Pascal Kray <57595818+krapas170@users.noreply.github.com>
2023-11-29 11:50:19 +0100

    Create Register1.js

---
+++ shop/src/sites/Register1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Register1() {
+  return (
+    <div>Register1Page
+      {/* Hier können Sie das Register1-Layout erstellen */}
+    </div>
+  );
+}
+
+export default Register1;
