9b60343b9b6c8bc2a780eebe0b0fdac9748952c1
Pascal Kray <57595818+krapas170@users.noreply.github.com>
2023-11-29 11:50:04 +0100

    Create GameAuswahl.js

---
+++ shop/src/sites/GameAuswahl.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function GameAuswahl() {
+  return (
+    <div>
+        GameAuswahlSeite
+      {/* Hier können Sie das Register1-Layout erstellen */}
+    </div>
+  );
+}
+
+export default GameAuswahl;
