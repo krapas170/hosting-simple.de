d1d63478cfeddd3363f2f36da43a1db2aac3c3c5
Pascal Kray <57595818+krapas170@users.noreply.github.com>
2023-12-01 17:16:04 +0100

    Update Header.js

--- shop/src/components/Header.js
+++ shop/src/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "%PUBLIC_URL%/logo.png";
+import logo from "../logo.png";
 
 function Header() {
   const shopStyle = {
